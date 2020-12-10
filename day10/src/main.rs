d040a04ab2c199b10190cf5acdc3871f95616e43
Atte Niemi <4998544+hur@users.noreply.github.com>
2020-12-10 12:00:24 +0000

    Apply clippy lints to day10
    We can use &[u32] instead of &Vec<u32> in function declarations
    and sort_unstable() is better than sort() for primitive types.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self, BufRead};
 use std::collections::HashMap;
 
-fn part1(data: &Vec<u32>) {
+fn part1(data: &[u32]) {
     // Diffs is a mapping of the magnitude of difference between two elements in data and the count
     // of the difference
     let mut diffs = HashMap::<u32,u32>::new();
@@ -15,7 +15,7 @@ fn part1(data: &Vec<u32>) {
     println!("Part 1: {}*{}={}", diffs.get(&1).unwrap(), diffs.get(&3).unwrap(), diffs.get(&1).unwrap() * diffs.get(&3).unwrap());
 }
 
-fn part2(data: &Vec<u32>) {
+fn part2(data: &[u32]) {
     // We describe the number of ways to chain _i_ adapters (steps[i]]) 
     // as the sum of ways to chain the previous adapters that can be connected to it
     // with steps[0] = 1
@@ -39,7 +39,7 @@ fn main() {
     let mut data: Vec<u32> = vec![0];
     data.extend(io::stdin().lock().lines()
         .map(|line| line.unwrap().parse::<u32>().unwrap()));
-    data.sort();
+    data.sort_unstable();
     data.push(&data[data.len() - 1] + 3); // Add the device joltage
     part1(&data);
     part2(&data);
